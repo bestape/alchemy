7d243ea721295f800d02b2d4e733d223f8df9269
bestape <git.kms@bestape.net>
2020-09-04 10:55:21 -0700

    more features; more images

--- app.js
+++ app.js
@@ -10,21 +10,72 @@ function animateFromTo( $0 ) {
 		+ '\t  dur="' + _0.speed + '"\n'
 		+ '\t  begin="' + $0.begin + '.end"\n'
 		+ '\t  fill="freeze" />\n'
+	;
 }
-function animateTransform( $0 ) {
-	return '  <animateTransform\n'
-		+ '\t  xlink:href="#rectangle' + $0.href + '"\n'
+function animateSize( _1, odd, cork ) {
+	_1.string += _0.animateFromTo( {
+		type: 'width'
+		, href: cork ? 'Infinity' : _0.globalCount
+		, from: cork ?
+			odd ? _0.corkSize.width * _1.xPowerMinus1
+			: _0.corkSize.height * _1.xPowerMinus1
+		: odd ? _0.a * _1.xPowerMinus1 : _0.b2 * _1.xPowerMinus1
+		, to: cork ?
+			odd ? _0.corkSize.width * _1.xPower
+			: _0.corkSize.height * _1.xPower
+		: odd ? _0.a * _1.xPower : _0.b2 * _1.xPower
+		, begin: _1.begin
+	} )
+		+ _0.animateFromTo( {
+			type: 'height'
+			, href: cork ? 'Infinity' : _0.globalCount
+			, from: cork ?
+				odd ? _0.corkSize.height * _1.xPowerMinus1
+				: _0.corkSize.width * _1.xPowerMinus1
+			: odd ? _0.b2 * _1.xPowerMinus1 : _0.a * _1.xPowerMinus1
+			, to: cork ?
+				odd ? _0.corkSize.height * _1.xPower
+				: _0.corkSize.width * _1.xPower
+			: odd ? _0.b2 * _1.xPower : _0.a * _1.xPower
+			, begin: _1.begin
+		} )
+	;
+}
+function animateTransform( _1, cork ) {
+	_1.string += '  <animateTransform\n'
+		+ '\t  xlink:href="#rectangle' + (
+			cork ? 'Infinity' : _0.globalCount
+		) + '"\n'
 		+ (
-			$0.globalId ? '\t  id="rectangle' + $0.globalId
-				+ 'animate' + $0.localId + '"\n' : ''
+			cork ? '' : '\t  id="rectangle' + _0.globalCount
+				+ 'animate' + _0.localCount + '"\n'
 		)
 		+ '\t  attributeName="transform"\n'
 		+ '\t  type="rotate"\n'
 		+ '\t  from="0, ' + _0.rotateCenter + '"\n'
 		+ '\t  to="-90, 0, 0"\n'
 		+ '\t  dur="' + _0.speed + '"\n'
-		+ '\t  begin="' + $0.begin + '.end"\n'
+		+ '\t  begin="' + _1.begin + '.end"\n'
 		+ '\t  fill="freeze" />\n'
+	;
+}
+function animateXY( _1, cork ) {
+	_1.string += _0.animateFromTo( {
+		type: 'x'
+		, href: cork ? 'Infinity' : _0.globalCount
+		, from: cork ? _1.thisRecord.corkX.from : _1.thisRecord.x.from
+		, to: cork ? _1.thisRecord.corkX.to : _1.thisRecord.x.to
+		, begin: _1.begin
+	} )
+		+ _0.animateFromTo( {
+			type: 'y'
+			, href: cork ? 'Infinity' : _0.globalCount
+			, from: cork ?
+				_1.thisRecord.corkY.from : _1.thisRecord.y.from
+			, to: cork ? _1.thisRecord.corkY.to : _1.thisRecord.y.to
+			, begin: _1.begin
+		} )
+	;
 }
 function dynamicObjects( $0 ) {
 	function next( $0 ) {
@@ -38,8 +89,22 @@ function dynamicObjects( $0 ) {
 	const _1 = new DynamicObjects( $0 )
 	; _1.count --
 	;
-	if ( _1.count + 1 ) {
+	if ( _1.count > -1 ) {
 		_0.writeFile( _0.file, _1.string, _0.writeOptions, _1.next )
+		; return
+		;
+	}
+	if ( _1.count > -2 && _0.cork ) {
+		_1.string = '  <rect\n'
+			+ '\t  id="rectangleInfinity"\n'
+			+ '\t  width="' + _0.corkSize.width + '"\n'
+			+ '\t  height="' + _0.corkSize.height + '"\n'
+			+ '\t  x="' + ( _0.borderSize / 2 ) + '"\n'
+			+ '\t  y="' + ( _0.b2 + _0.borderSize / 2 ) + '"\n'
+			+ '\t  stroke="' + _0.cellBorderColour + '"\n'
+			+ '\t  stroke-width="' + _0.cellBorderSize + '"\n'
+			+ '\t  fill="' + _0.cork + '" />\n'
+		; _0.writeFile( _0.file, _1.string, _0.writeOptions, _1.next )
 		; return
 		;
 	}
@@ -53,9 +118,12 @@ function dynamicObjects( $0 ) {
 			+ '\t  id="rectangle' + this.number + '"\n'
 			+ '\t  width="0"\n'
 			+ '\t  height="0"\n'
+			+ '\t  stroke="' + _0.cellBorderColour + '"\n'
+			+ '\t  stroke-width="' + _0.cellBorderSize + '"\n'
 			+ '\t  fill="' + _0.colours[
 				( this.number - 1 ) % _0.colours.length
 			] + '" />\n'
+		;
 	}
 }
 function footer() {
@@ -66,7 +134,9 @@ function footer() {
 			;
 		}
 		console.log(
-			'> log <\n', 'finished making ' + _0.file, '\n< log >'
+			'> log <\n'
+			, 'finished making ' + _0.file
+				+ ' with a ' + _0.x + ' scale invariant\n< log >'
 		)
 		;
 	}
@@ -101,6 +171,7 @@ function footer() {
 			+ '\t  dur="' + _0.speed + '"\n'
 			+ '\t  begin="resetAnimate.end"\n'
 			+ '\t  fill="freeze" />\n'
+		;
 	}
 	while ( _1.count < _0.repeat + 1 ) {
 		_1.string += '  <animate\n'
@@ -111,6 +182,16 @@ function footer() {
 			+ '\t  begin="resetAnimate.end"\n'
 			+ '\t  fill="freeze" />\n'
 		; _1.count ++
+		;
+	}
+	if ( _0.cork ) {
+		_1.string += '  <animate\n'
+			+ '\t  xlink:href="#rectangleInfinity"\n'
+			+ '\t  attributeName="width"\n'
+			+ '\t  values="0"\n'
+			+ '\t  dur="' + _0.speed + '"\n'
+			+ '\t  begin="resetAnimate.end"\n'
+			+ '\t  fill="freeze" />\n'
 		;
 	}
 	_1.string += '</svg>\n'
@@ -154,6 +235,10 @@ function header() {
 			+ '\t  height="' + _0.totalHeight + '"\n'
 			+ '\t  x="0"\n'
 			+ '\t  y="0"\n'
+			+ (
+				_0.background ?
+					'\t  fill="' + _0.background + '"\n' : ''
+			)
 			+ '\t  stroke="' + _0.borderColour + '"\n'
 			+ '\t  stroke-width="' + _0.borderSize + '" />\n'
 			+ '  <rect\n'
@@ -162,10 +247,11 @@ function header() {
 			+ '\t  height="' + _0.b2 + '"\n'
 			+ '\t  x="' + ( _0.borderSize / 2 ) + '"\n'
 			+ '\t  y="' + ( _0.borderSize / 2 ) + '"\n'
-			+ '\t  fill="' + (
-				_0.lead ? _0.colours[ _0.repeat - 1 ] : _0.colours[0]
-			) + '" />\n'
+			+ '\t  fill="' + _0.colours[ 0 ] + '"\n'
+			+ '\t  stroke="' + _0.cellBorderColour + '"\n'
+			+ '\t  stroke-width="' + _0.cellBorderSize + '" />\n'
 			+ '  <!-- dynamic objects -->\n'
+		;
 	}
 }
 function metalPythag() {
@@ -173,6 +259,11 @@ function metalPythag() {
 	; _0.x = ( _1.c - _1.b ) / _0.a
 	; _0.totalHeight = _0.b2 + ( _0.a * _0.x ) + _0.borderSize
 	;
+	if ( _0.cork ) {
+		_0.corkSize.width = ( _0.totalHeight - _0.borderSize ) * _0.x
+		; _0.corkSize.height = ( _0.totalWidth - _0.borderSize ) * _0.x
+		;
+	}
 	function MetalPythag() {
 		this.aSquared = Math.pow( _0.a, 2 )
 		; this.b = _0.b2 / 2
@@ -192,6 +283,14 @@ function part1() {
 			+ _0.b2 * _1.xPowerMinus5
 		; _1.record1.y.to = _1.record1.y.to
 			+ _0.b2 * _1.xPowerMinus4
+		; _1.record1.corkX.from = _1.record1.corkX.from
+			+ _0.b2 * _1.xPowerMinus4
+		; _1.record1.corkX.to = _1.record1.corkX.to
+			+ _0.b2 * _1.xPowerMinus3
+		; _1.record1.corkY.from = _1.record1.corkY.from
+			+ _0.b2 * _1.xPowerMinus1
+		; _1.record1.corkY.to = _1.record1.corkY.to
+			+ _0.b2 * _1.xPower
 		;
 	}
 	function next( $0 ) {
@@ -248,46 +347,27 @@ function part1() {
 				+ '\t  fill="freeze" />\n'
 			: ''
 		)
-		+ _0.animateFromTo( {
-			type: 'width'
-			, href: _0.globalCount
-			, from: _0.a * _1.xPowerMinus1
-			, to: _0.a * _1.xPower
-			, begin: _1.begin
-		} )
-		+ _0.animateFromTo( {
-			type: 'height'
-			, href: _0.globalCount
-			, from: _0.b2 * _1.xPowerMinus1
-			, to: _0.b2 * _1.xPower
-			, begin: _1.begin
-		} )
-		+ _0.animateFromTo( {
-			type: 'x'
-			, href: _0.globalCount
-			, from: _1.record1.x.from
-			, to: _1.record1.x.to
-			, begin: _1.begin
-		} )
-		+ _0.animateFromTo( {
-			type: 'y'
-			, href: _0.globalCount
-			, from: _1.record1.y.from
-			, to: _1.record1.y.to
-			, begin: _1.begin
-		} )
-		+ _0.animateTransform( {
-			href: _0.globalCount
-			, globalId: _0.globalCount
-			, localId: _0.localCount
-			, begin: _1.begin
-		} )
-	; _0.writeFile(	_0.file, _1.string, _0.writeOptions, _1.next )
+	;
+	if ( _0.globalCount == _0.localCount || ! _0.lead ) {
+		_0.animateSize( _1, true )
+		; _0.animateXY( _1 )
+		;
+		if ( _0.cork ) {
+			_0.animateSize( _1, true, true )
+			; _0.animateXY( _1, true )
+			; _0.animateTransform( _1, true )
+			;
+		}
+	}
+	_0.animateTransform( _1 )
+	; _0.writeFile( _0.file, _1.string, _0.writeOptions, _1.next )
 	;
 	function Part1() {
 		this.addToRecord = addToRecord
+		; this.start = _0.clickStart ?
+			'rectangle' + ( _0.repeat + 1) + '.click' : '0s'
 		; this.begin = _0.localCount < 5 ?
-			_0.globalCount < 2 || _0.lead ? '0s; endAnimate'
+			_0.globalCount < 2 || _0.lead ? this.start + '; endAnimate'
 			: 'rectangle' + ( _0.globalCount - 1 )
 			+ 'animate' + ( _0.globalCount - 1 )
 		: 'rectangle' + _0.globalCount
@@ -314,10 +394,25 @@ function part1() {
 				from: _0.borderSize / 2
 				, to: _0.borderSize / 2
 			}
+			, corkX: {
+				from: _0.borderSize / 2
+				, to: -(
+					_0.b2 * this.xPowerMinus1
+						+ _0.a * this.xPower
+						+ _0.borderSize / 2
+				)
+			}
+			, corkY: {
+				from: _0.b2 * this.xPowerMinus1
+					+_0.borderSize / 2
+				, to: _0.b2 * this.xPower
+					+ _0.borderSize / 2
+			}
 		}
 		; this.record2 = _0.thread.record2 ? _0.thread.record2 : null
 		; this.record3 = _0.thread.record3 ? _0.thread.record3 : null
 		; this.record4 = _0.thread.record4 ? _0.thread.record4 : null
+		; this.thisRecord = this.record1
 		;
 	}
 }
@@ -330,6 +425,14 @@ function part2() {
 		; _1.record2.y.from = _1.record2.y.from
 			+ _0.b2 * _1.xPowerMinus2
 		; _1.record2.y.to = _1.record2.y.to
+			+ _0.b2 * _1.xPowerMinus1
+		; _1.record2.corkX.from = _1.record2.corkX.from
+			+ _0.b2 * _1.xPowerMinus1
+		; _1.record2.corkX.to = _1.record2.corkX.to
+			+ _0.b2 * _1.xPower
+		; _1.record2.corkY.from = _1.record2.corkY.from
+			+ _0.b2 * _1.xPowerMinus2
+		; _1.record2.corkY.to = _1.record2.corkY.to
 			+ _0.b2 * _1.xPowerMinus1
 		;
 	}
@@ -370,40 +473,19 @@ function part2() {
 	}
 	_1.string += '  <!-- rectangle' + _0.globalCount
 		+ ' animate' + _0.localCount + ' -->\n'
-		+ _0.animateFromTo( {
-			type: 'width'
-			, href: _0.globalCount
-			, from: _0.b2 * _1.xPowerMinus1
-			, to: _0.b2 * _1.xPower
-			, begin: _1.begin
-		} )
-		+ _0.animateFromTo( {
-			type: 'height'
-			, href: _0.globalCount
-			, from: _0.a * _1.xPowerMinus1
-			, to: _0.a * _1.xPower
-			, begin: _1.begin
-		} )
-		+ _0.animateFromTo( {
-			type: 'x'
-			, href: _0.globalCount
-			, from: _1.record2.x.from
-			, to: _1.record2.x.to
-			, begin: _1.begin
-		} )
-		+ _0.animateFromTo( {
-			type: 'y'
-			, href: _0.globalCount
-			, from:_1.record2.y.from
-			, to: _1.record2.y.to
-			, begin: _1.begin
-		} )
-		+ _0.animateTransform( {
-			href: _0.globalCount
-			, globalId: _0.globalCount
-			, localId: _0.localCount
-			, begin: _1.begin
-		} )
+	;
+	if ( _0.globalCount == _0.localCount || ! _0.lead ) {
+		_0.animateSize( _1 )
+		; _0.animateXY( _1 )
+		;
+		if ( _0.cork ) {
+			_0.animateSize( _1, false, true )
+			; _0.animateXY( _1, true )
+			; _0.animateTransform( _1, true )
+			;
+		}
+	}
+	_0.animateTransform( _1 )
 	; _0.writeFile(	_0.file, _1.string, _0.writeOptions, _1.next )
 	;
 	function Part2() {
@@ -418,6 +500,7 @@ function part2() {
 		; this.xPowerMinus2 = Math.pow( _0.x, _0.localCount - 2 )
 		; this.xPowerMinus4 = Math.pow( _0.x, _0.localCount - 4 )
 		; this.xPowerMinus5 = Math.pow( _0.x, _0.localCount - 5 )
+		; this.xPowerPlus1 = Math.pow( _0.x, _0.localCount + 1 )
 		; this.record1 = _0.thread.record1
 		; this.record2 = _0.thread.record2 ? _0.thread.record2 : {
 			x: {
@@ -434,9 +517,25 @@ function part2() {
 				, to: _0.b2 * this.xPowerMinus1
 					+ _0.borderSize / 2
 			}
+			, corkX: {
+				from: _0.b2 * this.xPowerMinus1
+					+ _0.borderSize / 2
+				, to: -(
+					_0.b2 * this.xPowerMinus2
+						+ _0.a * this.xPowerPlus1
+						+ _0.borderSize / 2
+				)
+			}
+			, corkY: {
+				from: _0.b2 * this.xPowerMinus2
+					+_0.borderSize / 2
+				, to: _0.b2 * this.xPowerMinus1
+					+ _0.borderSize / 2
+			}
 		}
 		; this.record3 = _0.thread.record3 ? _0.thread.record3 : null
 		; this.record4 = _0.thread.record4 ? _0.thread.record4 : null
+		; this.thisRecord = this.record2
 		;
 	}
 }
@@ -450,6 +549,14 @@ function part3() {
 			- _0.b2 * _1.xPowerMinus1
 		; _1.record3.y.to = _1.record3.y.to
 			- _0.b2 * _1.xPower
+		; _1.record3.corkX.from = _1.record3.corkX.from
+			+ _0.b2 * _1.xPowerMinus2
+		; _1.record3.corkX.to = _1.record3.corkX.to
+			+ _0.b2 * _1.xPowerMinus1
+		; _1.record3.corkY.from = _1.record3.corkY.from
+			+ _0.b2 * _1.xPowerMinus3
+		; _1.record3.corkY.to = _1.record3.corkY.to
+			+ _0.b2 * _1.xPowerMinus2
 		;
 	}
 	function next( $0 ) {
@@ -489,40 +596,19 @@ function part3() {
 	}
 	_1.string +=  '<!-- rectangle' + _0.globalCount
 		+ ' animate' + _0.localCount + ' -->\n'
-		+ _0.animateFromTo( {
-			type: 'width'
-			, href: _0.globalCount
-			, from: _0.a * _1.xPowerMinus1
-			, to: _0.a * _1.xPower
-			, begin: _1.begin
-		} )
-		+ _0.animateFromTo( {
-			type: 'height'
-			, href: _0.globalCount
-			, from: _0.b2 * _1.xPowerMinus1
-			, to: _0.b2 * _1.xPower
-			, begin: _1.begin
-		} )
-		+ _0.animateFromTo( {
-			type: 'x'
-			, href: _0.globalCount
-			, from: _1.record3.x.from
-			, to: _1.record3.x.to
-			, begin: _1.begin
-		} )
-		+ _0.animateFromTo( {
-			type: 'y'
-			, href:_0.globalCount
-			, from:	_1.record3.y.from
-			, to: _1.record3.y.to
-			, begin: _1.begin
-		} )
-		+ _0.animateTransform( {
-			href: _0.globalCount
-			, globalId: _0.globalCount
-			, localId: _0.localCount
-			, begin: _1.begin
-		} )
+	;
+	if ( _0.globalCount == _0.localCount || ! _0.lead ) {
+		_0.animateSize( _1, true )
+		; _0.animateXY( _1 )
+		;
+		if ( _0.cork ) {
+			_0.animateSize( _1, true, true )
+			; _0.animateXY( _1, true )
+			; _0.animateTransform( _1, true )
+			;
+		}
+	}
+	_0.animateTransform( _1 )
 	; _0.writeFile(	_0.file, _1.string, _0.writeOptions, _1.next )
 	;
 	function Part3() {
@@ -556,8 +642,24 @@ function part3() {
 					+ _0.a * this.xPowerPlus1
 					+ _0.borderSize / 2
 			}
+			, corkX: {
+				from: _0.b2 * this.xPowerMinus2
+					+ _0.borderSize / 2
+				, to: -(
+					_0.b2 * this.xPowerMinus3
+						+ _0.a * this.xPower
+						+ _0.borderSize / 2
+				)
+			}
+			, corkY: {
+				from: _0.b2 * this.xPowerMinus3
+					+ _0.borderSize / 2
+				, to: _0.b2 * this.xPowerMinus2
+					+ _0.borderSize / 2
+			}
 		}
 		; this.record4 = _0.thread.record4 ? _0.thread.record4 : null
+		; this.thisRecord = this.record3
 		;
 	}
 }
@@ -570,6 +672,14 @@ function part4() {
 		; _1.record4.y.from = _1.record4.y.from
 			+ _0.b2 * _1.xPowerMinus4
 		; _1.record4.y.to = _1.record4.y.to
+			+ _0.b2 * _1.xPowerMinus3
+		; _1.record4.corkX.from = _1.record4.corkX.from
+			+ _0.b2 * _1.xPowerMinus3
+		; _1.record4.corkX.to = _1.record4.corkX.to
+			+ _0.b2 * _1.xPowerMinus2
+		; _1.record4.corkY.from = _1.record4.corkY.from
+			+ _0.b2 * _1.xPowerMinus4
+		; _1.record4.corkY.to = _1.record4.corkY.to
 			+ _0.b2 * _1.xPowerMinus3
 		;
 	}
@@ -610,40 +720,19 @@ function part4() {
 	}
 	_1.string += '  <!-- rectangle' + _0.globalCount
 		+ ' animate' + _0.localCount + ' -->\n'
-		+ _0.animateFromTo( {
-			type: 'width'
-			, href: _0.globalCount
-			, from: _0.b2 * _1.xPowerMinus1
-			, to: _0.b2 * _1.xPower
-			, begin: _1.begin
-		} )
-		+ _0.animateFromTo( {
-			type: 'height'
-			, href: _0.globalCount
-			, from: _0.a * _1.xPowerMinus1
-			, to: _0.a * _1.xPower
-			, begin: _1.begin
-		} )
-		+ _0.animateFromTo( {
-			type: 'x'
-			, href: _0.globalCount
-			, from: _1.record4.x.from
-			, to: _1.record4.x.to
-			, begin: _1.begin
-		} )
-		+ _0.animateFromTo( {
-			type: 'y'
-			, href:_0.globalCount
-			, from:	_1.record4.y.from
-			, to: _1.record4.y.to
-			, begin: _1.begin
-		} )
-		+ _0.animateTransform( {
-			href: _0.globalCount
-			, globalId: _0.globalCount
-			, localId: _0.localCount
-			, begin: _1.begin
-		} )
+	;
+	if ( _0.globalCount == _0.localCount || ! _0.lead ) {
+		_0.animateSize( _1 )
+		; _0.animateXY( _1 )
+		;
+		if ( _0.cork ) {
+			_0.animateSize( _1, false, true )
+			; _0.animateXY( _1, true )
+			; _0.animateTransform( _1, true )
+			;
+		}
+	}
+	_0.animateTransform( _1 )
 	; _0.writeFile( _0.file, _1.string, _0.writeOptions, _1.next )
 	;
 	function Part4() {
@@ -654,6 +743,7 @@ function part4() {
 		; this.string = ''
 		; this.xPower = Math.pow( _0.x, _0.localCount )
 		; this.xPowerMinus1 = Math.pow( _0.x, _0.localCount - 1 )
+		; this.xPowerMinus2 = Math.pow( _0.x, _0.localCount - 2 )
 		; this.xPowerMinus3 = Math.pow( _0.x, _0.localCount - 3 )
 		; this.xPowerMinus4 = Math.pow( _0.x, _0.localCount - 4 )
 		; this.record1 = _0.thread.record1
@@ -676,7 +766,23 @@ function part4() {
 				, to: _0.b2 * this.xPowerMinus3
 					+ _0.borderSize / 2
 			}
+			, corkX: {
+				from: _0.b2 * this.xPowerMinus3
+					+ _0.borderSize / 2
+				, to: -(
+					_0.b2 * this.xPowerMinus4
+						+ _0.a * this.xPowerMinus1
+						+ _0.borderSize / 2
+				)
+			}
+			, corkY: {
+				from: _0.b2 * this.xPowerMinus4
+					+ _0.borderSize / 2
+				, to: _0.b2 * this.xPowerMinus3
+					+ _0.borderSize / 2
+			}
 		}
+		; this.thisRecord = this.record4
 		;
 	}
 }
@@ -687,11 +793,23 @@ const _0 = new App
 function App() {
 	this.data = require( './app.json' )
 	; this.a = this.data.a
+//	; this.a = 990000/199
 	; this.animateFromTo = animateFromTo
+	; this.animateSize = animateSize
 	; this.animateTransform = animateTransform
+	; this.animateXY = animateXY
 	; this.b2 = this.data.b2
+	; this.background = this.data.background
 	; this.borderColour = this.data.borderColour
 	; this.borderSize = this.data.borderSize
+	; this.cellBorderColour = this.data.cellBorderColour
+	; this.cellBorderSize = this.data.cellBorderSize
+	; this.clickStart = this.data.clickStart
+	; this.cork = this.data.cork
+	; this.corkSize = {
+		width: null
+		, height: null
+	}
 	; this.colours = this.data.colours
 	; this.dynamicObjects = dynamicObjects
 	; this.file = this.data.file
